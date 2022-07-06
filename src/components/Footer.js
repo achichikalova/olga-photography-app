a8f26db2a313881c4f0760c5ed2fd46c95e98daf
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-07-06 13:30:03 -0400

    Implement footer component, add icons for socials, and style it

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Footer.scss";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaFacebookF, FaPinterestP } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="icons">
+        <AiOutlineInstagram className="icon" />
+        <FaFacebookF className="icon" />
+        <FaPinterestP className="icon" />
+      </div>
+      <p>All content Copyright © 2022 Olga Photography</p>
+    </footer>
+  );
 };
 
 export default Footer;
