6361853778e64a8b1b6c3943c463782c31afe5b5
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-07-06 10:46:36 -0400

    Create footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import About from "./pages/About";
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
