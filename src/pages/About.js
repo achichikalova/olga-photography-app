dc61d8249884937e239bba9140e75ccd5810516e
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-07-05 21:57:08 -0400

    Create About component

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Olga from "../assets/images/about.jpg";
+
+const About = () => {
+  return (
+    <div>
+      <image src={Olga} />
+      <p>
+        Hi there! My name is Olga. And i am a lifestyle photographer. I am
+        married and i have two kids. I am nature lover. Photography is my life.
+        When I go outside, I always take my camera with me. I like people,
+        smiles and sincerity. Each of you is unique — this is what I want to
+        show trough my pictures, so I have individual approach to each shooting
+        and to each and every of you. Do not be shy, just be yourself and we
+        will take some amazing pictures.
+      </p>
+    </div>
+  );
+};
+
+export default About;
