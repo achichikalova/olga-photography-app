89dee0f2ae5150661d5839f57eb105925f6fcb52
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-07-06 10:47:57 -0400

    Implement About page and style it

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,37 @@
 import React from "react";
 import Olga from "../assets/images/about.jpg";
+import "./About.scss";
 
 const About = () => {
   return (
-    <div>
-      <image src={Olga} />
-      <p>
-        Hi there! My name is Olga. And i am a lifestyle photographer. I am
-        married and i have two kids. I am nature lover. Photography is my life.
-        When I go outside, I always take my camera with me. I like people,
-        smiles and sincerity. Each of you is unique — this is what I want to
-        show trough my pictures, so I have individual approach to each shooting
-        and to each and every of you. Do not be shy, just be yourself and we
-        will take some amazing pictures.
-      </p>
+    <div className="about">
+      <div className="intro">
+        <span>
+          Hey,
+          <br />
+          I'm Olga
+        </span>
+      </div>
+      <div className="about-info">
+        <article>
+          <p>Hi there!</p>
+          <p>
+            I'm so glad you're here! I'm Olga, a lifestyle photographer. I'm
+            married and have two kids. I'm a nature lover.
+          </p>
+          <p>
+            Photography is my life. When I go outside, I always take my camera
+            with me. I like people, smiles, and sincerity. I love to hang out
+            with my boys while I'm not shooting!
+          </p>
+          <p>
+            Each of you is unique — this is what I want to show through my
+            pictures, so I have an individual approach to each shooting. Don't
+            be shy. Just be yourself, and we'll take some fantastic pictures.
+          </p>
+        </article>
+        <img src={Olga} alt="my photo" />
+      </div>
     </div>
   );
 };
