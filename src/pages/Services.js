3b3319d1461a5502a9e8c95f8ad402d9c74e7987
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-07-08 22:03:11 -0400

    WIP: Continue work on the page and style, add services data, render service card component based on it

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,72 @@
 import React from "react";
+import ServiceCard from "../components/ServiceCard";
+import "./Services.scss";
+
+const servicesData = [
+  {
+    title: "Individual",
+    price: 200,
+    includes: [
+      "up to 1 - 1.5 hour",
+      "1 location",
+      "1 outfit",
+      "20+ edited pictures (online gallery)",
+      "up 5-7 days editing time",
+    ],
+    additional:
+      "Photoshoot includes assistance in choosing outfits, location, idea.",
+  },
+  {
+    title: "Couple",
+    price: 250,
+    includes: [
+      "up to 1 - 1.5 hour",
+      "1 location",
+      "1 outfit",
+      "20+ edited pictures (online gallery)",
+      "up 5-7 days editing time",
+    ],
+    additional:
+      "Photoshoot includes assistance in choosing outfits, location, idea.",
+  },
+  {
+    title: "Family",
+    price: 250,
+    includes: [
+      "up 1.5 hour",
+      "1 location",
+      "1 outfit",
+      "25+ edited pictures (online gallery)",
+      "up 5-7 days editing time",
+    ],
+    additional:
+      "Photoshoot includes assistance in choosing outfits, location, idea.",
+  },
+  {
+    title: "Newborn",
+    price: 250,
+    includes: [
+      "up 2 hour",
+      "shooting at your place (in home lifestyle photoshoot)",
+      "up to 2 outfits",
+      "25-30 edited pictures (online gallery)",
+      "up 5-7 days editing time",
+    ],
+  },
+];
 
 const Services = () => {
-  return <div>Services</div>;
+  const services = servicesData.map((service, index) => {
+    return <ServiceCard {...service} key={index} />;
+  });
+
+  return (
+    <section className="services">
+      <h1>Services</h1>
+      <div className="service">{services}</div>
+      <p>* {servicesData[0].additional}</p>
+    </section>
+  );
 };
 
 export default Services;
