27cfebfeaa009bd62dbb13905c33651442c2902f
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-07-11 13:37:50 -0400

    Add footer to the layout, create routes for Home and Services pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
 import About from "./pages/About";
+import Footer from "./components/Footer";
+import Services from "./pages/Services";
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
         <Route path="/about" element={<About />} />
+      </Routes>
+      <Routes>
+        <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
     </BrowserRouter>
