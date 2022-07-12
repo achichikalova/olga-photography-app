504c5619754159c579728316610c07ef76084511
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-07-12 15:26:18 -0400

    Implement carousel for home page and fix styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,30 @@
 import React from "react";
+import Carousel from "framer-motion-carousel";
 import "./Home.scss";
 import Family from "../assets/images/family/9.jpg";
-import Parallax from "../assets/images/couple/8.jpg";
+import Family2 from "../assets/images/family/10.jpg";
+import Couple from "../assets/images/couple/8.jpg";
+import Couple2 from "../assets/images/couple/10.jpg";
+import Individual from "../assets/images/individual/7.jpg";
+import Individual2 from "../assets/images/individual/4.jpg";
 
 const Home = () => {
+  const carousel = [Family2, Couple, Individual, Couple2, Individual2];
   return (
     <section className="home">
       <div className="parallax">
-        <img src={Parallax} alt="parallax" />
+        <Carousel
+          interval={10000}
+          renderDots={({ activeIndex, setActiveIndex }) => <div />}
+        >
+          {carousel.map((item, i) => {
+            return <img src={item} alt="parallax" key={i} draggable="false" />;
+          })}
+        </Carousel>
       </div>
+
       <div className="info">
         <div className="quote">
-          {/* <p>
-            "LIVE FOR
-            <br />
-            THE <em>MOMENTS</em>
-            <br />
-            YOU CAN'T PUT
-            <br />
-            INTO <em>WORDS</em>"
-          </p> */}
           <p>
             "Each of you is <em>unique</em> —
             <br />
