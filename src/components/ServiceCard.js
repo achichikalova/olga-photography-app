1525b04b950dd073fc4e799e702798bff35590cd
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-07-13 12:23:08 -0400

    Add link to the card to the gallery page based on gallery theme

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./ServiceCard.scss";
 
 const ServiceCard = ({ title, price, includes, additional }) => {
@@ -6,15 +7,19 @@ const ServiceCard = ({ title, price, includes, additional }) => {
     return <li>{item}</li>;
   });
 
+  const theme = title.toLowerCase();
+
   return (
-    <div className="service-card">
-      <h2>{title}</h2>
-      <ul>{includesList}</ul>
-      <h3>
-        Price ${price}
-        <span>{additional && "*"}</span>
-      </h3>
-    </div>
+    <Link to={`/services/${theme}`} className="service">
+      <div className="service-card">
+        <h2>{title}</h2>
+        <ul>{includesList}</ul>
+        <h3>
+          Price ${price}
+          <span>{additional && "*"}</span>
+        </h3>
+      </div>
+    </Link>
   );
 };
 
