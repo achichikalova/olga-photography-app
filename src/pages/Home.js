f2df90c4254327ac7439ff467493406628b379f5
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-07-20 10:14:35 -0400

    Change carousel component, fetch images from storage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,50 @@
-import React from "react";
-import Carousel from "framer-motion-carousel";
+import React, { useEffect, useRef, useState } from "react";
+import { getDownloadURL, listAll, ref } from "firebase/storage";
+import Family from "../assets/images/9.jpg";
+import ImageGallery from "react-image-gallery";
+import { storage } from "../firebase/config";
 import "./Home.scss";
-import Family from "../assets/images/family/9.jpg";
-import Family2 from "../assets/images/family/10.jpg";
-import Couple from "../assets/images/couple/8.jpg";
-import Couple2 from "../assets/images/couple/10.jpg";
-import Individual from "../assets/images/individual/7.jpg";
-import Individual2 from "../assets/images/individual/4.jpg";
 
 const Home = () => {
-  const carousel = [Family2, Couple, Individual, Couple2, Individual2];
+  const [images, setImages] = useState([]);
+
+  const imagesRef = ref(storage, `/random`);
+  const shouldFetch = useRef(true);
+
+  useEffect(() => {
+    if (shouldFetch.current) {
+      shouldFetch.current = false;
+      listAll(imagesRef).then((res) => {
+        console.log(res);
+        res.items
+          .forEach((item) => {
+            getDownloadURL(item).then((url) => {
+              console.log(url);
+              setImages((prev) => [...prev, { original: url, thumbnail: url }]);
+            });
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+    }
+  }, [imagesRef]);
+
+  console.log(images);
+
   return (
     <section className="home">
       <div className="carousel">
-        <Carousel
-          interval={5000}
-          renderArrowLeft={({ activeIndex, setActiveIndex }) => <div />}
-          renderArrowRight={({ activeIndex, setActiveIndex }) => <div />}
-          renderDots={({ activeIndex, setActiveIndex }) => <div />}
-        >
-          {carousel.map((item, i) => {
-            return <img src={item} alt="carousel" key={i} draggable="false" />;
-          })}
-        </Carousel>
+        <ImageGallery
+          autoPlay={true}
+          showIndex={true}
+          showThumbnails={false}
+          slideDuration={800}
+          slideInterval={5000}
+          items={images}
+          showFullscreenButton={false}
+          showBullets={true}
+        />
       </div>
       <div className="info">
         <div className="quote">
